9619e302e7a0d335845348bae770cadb8a8c9365
kirillzyusko <zyusko.kirik@gmail.com>
2023-08-09 16:23:12 +0200

    refactor: test view removal

--- FabricExample/src/screens/Examples/AwareScrollView/index.tsx
+++ FabricExample/src/screens/Examples/AwareScrollView/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { Button, TextInput } from 'react-native';
 import { useResizeMode } from 'react-native-keyboard-controller';
 
 import { randomColor } from '../../../utils';
@@ -9,23 +9,30 @@ import { styles } from './styles';
 
 export default function AwareScrollView() {
   useResizeMode();
+  const [items, setItems] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   return (
     <>
-      {new Array(10).fill(0).map((_, i) => (
-        <TextInput
-          key={i}
-          placeholder={`${i}`}
-          placeholderTextColor="black"
-          multiline
-          style={{
-            width: '100%',
-            minHeight: 50,
-            // maxHeight: 250,
-            backgroundColor: randomColor(),
-            marginTop: 50,
-          }}
-        />
+      {items.map((i) => (
+        <>
+          <TextInput
+            key={i}
+            placeholder={`${i}`}
+            placeholderTextColor="black"
+            multiline
+            style={{
+              width: '100%',
+              minHeight: 50,
+              // maxHeight: 250,
+              backgroundColor: randomColor(),
+              marginTop: 50,
+            }}
+          />
+          <Button
+            title={`remove ${i}`}
+            onPress={() => setItems(items.filter((id) => id !== i))}
+          />
+        </>
       ))}
     </>
   );
